ccc2ad72495d9f9b087ab506dc2b6d5a5eaef808
Brent Nkomo <brent.nkomo@gmail.com>
2016-07-20 19:38:24 -0600

    actually add bootload aws script; bower setup;

---
+++ bootload_script.sh
@@ -0,0 +1,7 @@
+cd
+sudo apt update -y
+sudo apt upgrade -y
+wget http://repo.continuum.io/miniconda/Miniconda3-3.7.0-Linux-x86_64.sh -O ~/miniconda.sh
+bash ~/miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+conda install -y numpy scipy flask
