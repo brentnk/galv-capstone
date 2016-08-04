1db1ba9bb69b60c19738db25c50db3fa28b15bfe
Brent Nkomo <brent.nkomo@gmail.com>
2016-08-03 19:55:34 -0600

    minor ui adjustments; todo hex highlight;

--- website/js/index.js
+++ website/js/index.js
@@ -13,15 +13,19 @@ var map = L.map("map-canvas",{
   minZoom:10,
   maxZoom:11
 });
-L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',
-  { attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://cartodb.com/attributions">CartoDB</a>' }
-).addTo(map);
+// L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',
+//   { attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://cartodb.com/attributions">CartoDB</a>' }
+// ).addTo(map);
+
+L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
 var valueAlgorithm = 'sumScaledCounter';
 var options = {
-  radiusRange: [11,11],
-  radius: 11,
-  opacity: 0.47,
+  radiusRange: [8,8],
+  radius: 8,
+  opacity: 0.57,
   lng: function(d){ return d[1]; },
   lat: function(d){ return d[2]; },
   value: uniqueCellCounter,
@@ -30,8 +34,8 @@ var options = {
 }
 
 var colorScales = [
-  d3.interpolateViridis,
-  d3.interpolateInferno
+  d3.interpolateViridis
+  // d3.interpolateInferno
 ]
 
 var hexOverlay = L.hexbinLayer(options);
